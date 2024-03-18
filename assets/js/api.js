704ad0e1149f07595444f579615e44c7aaadb012
Nishant Thakre <130768503+NishantThakre@users.noreply.github.com>
2024-03-18 15:08:21 +0530

    Weatherio app

---
+++ assets/js/api.js
@@ -0,0 +1,42 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright NemoKamado 2023 All rights reserved
+ * @author nemoKamado
+ */
+
+'use strict';
+
+const api_key = "904830e6be2bdc1f1f782b3207a1d1ae";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+  },
+  /**
+   * @param {string} query Search query e.g.: "London", "New York"
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+  }
+}
